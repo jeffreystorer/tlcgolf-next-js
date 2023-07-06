7155ef35793554d03bbad8ac7bc5b6fcca365e70
jeffreystorer <jeffrey.storer@gmail.com>
2023-07-05 20:40:00 -0400

    7/5: Land on Home Page No Navbar

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,14 +1,14 @@
 //import { RecoilRootWrapper } from '@/wrappers/recoilroutewrapper';
-import { NavBar } from '@/components/common/navbar';
+//import { NavBar } from '@/components/common/navbar';
 import '@/app/globals.css';
 
 export default function HomeLayout({ children }) {
   return (
     <>
     {/* <RecoilRootWrapper> */}
-      <header>
+     {/*  <header>
         <NavBar />
-      </header>
+      </header> */}
       <main>{children}</main>
     {/* </RecoilRootWrapper> */}  
    </>

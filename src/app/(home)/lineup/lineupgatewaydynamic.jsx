409de3130e8d4794aafa0f502408eadadbe20f3e
jeffreystorer <jeffrey.storer@gmail.com>
2023-08-07 19:05:08 -0400

    8/7: Add lineupgatewaydynamic

---
+++ src/app/(home)/lineup/lineupgatewaydynamic.jsx
@@ -0,0 +1,17 @@
+'use client';
+import dynamic from 'next/dynamic';
+const LineupGateway = dynamic(() => import('@/app/(home)/lineup/lineupgateway'), {
+  ssr: false,
+});
+import { useRouter } from 'next/navigation';
+import { sget } from '@/components/common/utils'
+
+export default function LineupGatewayDynamic() {
+  const router = useRouter();
+  const isLoggedIn = sget('isLoggedIn');
+  if (!isLoggedIn) {
+    router.push('/');
+    return false;
+  }
+  return <LineupGateway />;
+}
--- src/app/(home)/lineup/page.jsx
+++ src/app/(home)/lineup/page.jsx
@@ -1,17 +1,9 @@
 'use client';
 import dynamic from 'next/dynamic';
-const LineupGateway = dynamic(() => import('@/app/(home)/lineup/lineupgateway'), {
+const LineupGatewayDynamic = dynamic(() => import('@/app/(home)/lineup/lineupgatewaydynamic'), {
   ssr: false,
 });
-import { useRouter } from 'next/navigation';
-import { sget } from '@/components/common/utils'
 
-export default function Page() {
-  const router = useRouter();
-  const isLoggedIn = sget('isLoggedIn');
-  if (!isLoggedIn) {
-    router.push('/');
-    return false;
-  }
-  return <LineupGateway />;
+export default function Page() {  
+  return <LineupGatewayDynamic />;
 }

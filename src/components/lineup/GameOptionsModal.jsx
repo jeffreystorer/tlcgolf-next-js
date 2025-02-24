f1ce2059cfcad7b8c2cfc7181286a2efcb7844ad
jeffreystorer <jeffrey.storer@gmail.com>
2025-02-23 20:44:51 -0500

    2/23 Working on State

--- src/components/lineup/GameOptionsModal.jsx
+++ src/components/lineup/GameOptionsModal.jsx
@@ -14,16 +14,26 @@ import * as state from '@/store';
 //TODO: Fix failure to remember bet
 
 export default function GameOptionsModal() {
-  const [holes, setHoles] = useState('6/6/6');
+  const betsArray = useRecoilValue(state.bets);
+  const betsOptionItems = betsArray.map((item) => (
+    <option key={uuidv4()} value={item}>
+      {item}
+    </option>
+  ));
+  const [holes, setHoles] = useState('');
+  const [bet, setBet] = useState('');
+  const [maxValue, setMaxValue] = useState(false);
+  const [grossup, setGrossup] = useState('');
   const [entry, setEntry] = useState(0);
   const [entryPer, setEntryPer] = useState('/player');
   const [pot, setPot] = useState(0);
-  const [potPer, setPotPer] = useState('0.00');
   const [remainder, setRemainder] = useState(0);
   const [remPer, setRemPer] = useState('0.00');
   const [firstPayout, setFirstPayout] = useState(0);
   const [secondPayout, setSecondPayout] = useState(0);
   const [thirdPayout, setThirdPayout] = useState(0);
+  const [rules, setRules] = useState('');
+  const [putts, setPutts] = useState('');
 
   const setTextareaValue = useSetRecoilState(state.textareaValue);
   const teamTables = useRecoilValue(state.teamTables)
@@ -31,12 +41,6 @@ export default function GameOptionsModal() {
   const excessPayoutMessage =
     'You are paying out more than the pot.  Please adjust your payouts.';
   const missingHolesMessage = 'Please select the number of holes for each bet.';
-  const betsArray = useRecoilValue(state.bets);
-  const betsOptionItems = betsArray.map((item) => (
-    <option key={uuidv4()} value={item}>
-      {item}
-    </option>
-  ));
   
   const playerCount = () => {
     let teamCount = Object.keys(teamTables).length - 1;
@@ -59,58 +63,54 @@ export default function GameOptionsModal() {
     return teamPlayerCount;
   }
 
+  function handleBetChange(e) {
+    setBet(e.target.value);
+  }
+
   function handleHolesChange(e) {
     const newHoles = e.target.value;
     setHoles(newHoles);
-    setPotPer(computePotPer(newHoles, pot).toFixed(2));
     const newRemainder = computeRemainder(newHoles, pot, firstPayout, secondPayout, thirdPayout);
     setRemainder(newRemainder);
     setRemPer(computeRemPer(newHoles, newRemainder).toFixed(2))
   }
 
+  function handleMaxValueChange(e) {
+    setMaxValue(e.target.checked);
+  }
+
+  function handleGrossupChange(e) {
+    setGrossup(e.target.value);
+  }
+
   function handleEntryPerChange(e) {
-    const newEntryPer = e.target.value;
-    setEntryPer(newEntryPer);
-    const newPot = computePot(entry, newEntryPer);
-    setPot(newPot);
-    setPotPer(computePotPer(holes, newPot).toFixed(2));
-    const newRemainder = computeRemainder(holes, newPot, firstPayout, secondPayout, thirdPayout);
-    setRemainder(newRemainder);
-    setRemPer(computeRemPer(holes, newRemainder).toFixed(2))
+    setEntryPer(e.target.value);
   }
 
   function handleEntryChange(e) {
     const newEntry = Number(e.target.value);
-    console.log(newEntry);
     setEntry(newEntry);
     const newPot = computePot(newEntry, entryPer);
-    console.log('newPot: ',newPot);
     setPot(newPot);
-    setPotPer(computePotPer(holes, newPot).toFixed(2))
     const newRemainder = computeRemainder(holes, newPot, firstPayout, secondPayout, thirdPayout);
-    console.log(holes, newPot, firstPayout, secondPayout, thirdPayout);
-    console.log('newRemainder: ',newRemainder);
     setRemainder(newRemainder);
     setRemPer(computeRemPer(holes, newRemainder).toFixed(2))
-    console.log('remPer: ',remPer);
+    setRemainder(newRemainder);
+    setRemPer(computeRemPer(holes, newRemainder).toFixed(2))
   }
 
   function handleFirstPayoutChange(e) {
     const newFirstPayout = Number(e.target.value);
-    console.log('newFirstPayout: ',newFirstPayout);
     setFirstPayout(newFirstPayout);
     const newRemainder = computeRemainder(holes, pot, newFirstPayout, secondPayout, thirdPayout);
-    console.log('newRemainder: ',newRemainder);
     setRemainder(newRemainder);
     setRemPer(computeRemPer(holes, newRemainder).toFixed(2))
   }
 
   function handleSecondPayoutChange(e) {
     const newSecondPayout = Number(e.target.value);
-    console.log('newSecondPayout: ',newSecondPayout);
     setSecondPayout(newSecondPayout);
     const newRemainder = computeRemainder(holes, pot, firstPayout, newSecondPayout, thirdPayout);
-    console.log('newRemainder: ',newRemainder);
     setRemainder(newRemainder);
     setRemPer(computeRemPer(holes, newRemainder).toFixed(2))
   }
@@ -119,53 +119,16 @@ export default function GameOptionsModal() {
     const newThirdPayout = Number(e.target.value);
     setThirdPayout(newThirdPayout);
     const newRemainder = computeRemainder(holes, pot, firstPayout, secondPayout, newThirdPayout);
-    console.log('newRemainder: ',newRemainder);
     setRemainder(newRemainder);
     setRemPer(computeRemPer(holes, newRemainder).toFixed(2))
   } 
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
-    const bet = formJson.bet;
-    const maxValue = formJson.max;
-    let max = '';
-    if (maxValue) max = 'Net double bogey max.';
-    const grossup = formJson.grossup;
-    const rules = formJson.rules;
-    const putts = formJson.putts;
-    if (holes !== '6/6/6' && holes !== '9&9' && holes !== '18') {
-      alert(missingHolesMessage);
-      return;
-    }
-    if (remainder < 0) {
-      alert(excessPayoutMessage);
-      return;
-    }
-    let textareaValue = holes + ' ' + bet;
-    if (max !== '') textareaValue = textareaValue + '\n' + max;
-    if (grossup !== '') textareaValue = textareaValue + '\n' + grossup;
-    textareaValue =
-      textareaValue +
-      '\n' +
-      'Entry: $' +
-      entry +
-      entryPer +
-      '  Pot: $' +
-      pot;
-    if (thirdPayout > 0) textareaValue = textareaValue + '\n';
-    textareaValue = textareaValue + '\nPayout: $' + firstPayout;
-    if (secondPayout > 0) textareaValue = textareaValue + '/$' + secondPayout;
-    if (thirdPayout > 0) textareaValue = textareaValue + '/$' + thirdPayout;
-    if (remainder > 0)
-      textareaValue =
-        textareaValue + '\n$' + remainder + ' for _________';
-    if (rules !== '') textareaValue = textareaValue + '\n' + rules;
-    if (putts !== '') textareaValue = textareaValue + '\n' + putts;
-    setTextareaValue((prev) => textareaValue);
-    window.location.href = '#';
+  function handleRulesChange(e) {
+  setRules(e.target.value);
+  }
+
+  function handlePuttsChange(e) {
+  setPutts(e.target.value);
   }
 
   function computePot(entry, entryPer) {
@@ -180,24 +143,7 @@ export default function GameOptionsModal() {
       default:
         break;
     }
-  }
-  
-
-  function computePotPer(
-    holes,
-    pot
-  ) {
-    switch (holes) {
-      case '6/6/6':
-        return pot/3;
-      case '9&9':
-        return pot/2;
-      case '18':
-        return pot;
-      default:
-        break;
-    }
-  }
+  }  
 
   function computeRemainder(
     holes,
@@ -235,6 +181,42 @@ export default function GameOptionsModal() {
     }
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    let max = '';
+    if (maxValue) max = 'Net double bogey max.';
+    if (holes !== '6/6/6' && holes !== '9&9' && holes !== '18') {
+      alert(missingHolesMessage);
+      return;
+    }
+    if (remainder < 0) {
+      alert(excessPayoutMessage);
+      return;
+    }
+    let textareaValue = holes + ' ' + bet;
+    if (max !== '') textareaValue = textareaValue + '\n' + max;
+    if (grossup !== '') textareaValue = textareaValue + '\n' + grossup;
+    textareaValue =
+      textareaValue +
+      '\n' +
+      'Entry: $' +
+      entry +
+      entryPer +
+      '  Pot: $' +
+      pot;
+    if (thirdPayout > 0) textareaValue = textareaValue + '\n';
+    textareaValue = textareaValue + '\nPayout: $' + firstPayout;
+    if (secondPayout > 0) textareaValue = textareaValue + '/$' + secondPayout;
+    if (thirdPayout > 0) textareaValue = textareaValue + '/$' + thirdPayout;
+    if (remainder > 0)
+      textareaValue =
+        textareaValue + '\n$' + remainder + ' for _________';
+    if (rules !== '') textareaValue = textareaValue + '\n' + rules;
+    if (putts !== '') textareaValue = textareaValue + '\n' + putts;
+    setTextareaValue((prev) => textareaValue);
+    window.location.href = '#';
+  }
+
   return (
     <div id='gameoptionsmodal' className='modal'>
       <a href='#' className='modalClose' hidden></a>
@@ -245,30 +227,30 @@ export default function GameOptionsModal() {
         </header>
         <form onSubmit={handleSubmit}>
           <fieldset>
-            <select name='holes' onChange={handleHolesChange}>
+            <select name='holes' onChange={handleHolesChange} value={holes}>
               <option value=''>Select Number of Holes for Each Bet</option>
               {holesOptionItems}
             </select>
-            <select name='bet'>
+            <select name='bet' onChange={handleBetChange} value={bet}>
               <option value=''>Select Bet</option>
               {betsOptionItems}
             </select>
             <label>
-              <input type='checkbox' name='max' />
+              <input type='checkbox' name='max' onChange={handleMaxValueChange} checked={maxValue}/>
               Net double bogey max.?
             </label>
-            <select name='grossup'>
+            <select name='grossup' onChange={handleGrossupChange} value={grossup}>
               <option value=''>Gross Up?</option>
               {grossupOptionItems}
             </select>
-            <select name='entryPer' onChange={handleEntryPerChange}>
-              <option value='/player'>Entry per player or team?</option>
+            <select name='entryPer' onChange={handleEntryPerChange} value={entryPer}>
+              <option value='/player'>Entry per player or team?</option>  
               {entryPerOptionItems}
             </select>
             <p>Entry:</p>
             <article>
               <label>
-                <input type='number' name='entry' min='1' max='100' onChange={handleEntryChange} />
+                <input type='number' name='entry' min='1' max='100' onChange={handleEntryChange} value={entry} />
               </label>          
               {entry > 0 &&
                 <p>Pot: {pot}</p>
@@ -279,17 +261,17 @@ export default function GameOptionsModal() {
                 <label>
                   1st:
                   <br />
-                  <input type='number' name='firstPayout' min='1' max='100' onChange={handleFirstPayoutChange} />
+                  <input type='number' name='firstPayout' min='1' max='100' onChange={handleFirstPayoutChange} value={firstPayout} />
                 </label>
                 <label>
                   2nd:
                   <br />
-                  <input type='number' name='secondPayout' min='1' max='100' onChange={handleSecondPayoutChange} />
+                  <input type='number' name='secondPayout' min='1' max='100' onChange={handleSecondPayoutChange} value={secondPayout} />
                 </label>
                 <label>
                   3rd:
                   <br />
-                  <input type='number' name='thirdPayout' min='1' max='100' onChange={handleThirdPayoutChange} />
+                  <input type='number' name='thirdPayout' min='1' max='100' onChange={handleThirdPayoutChange} value={thirdPayout} />
                 </label>       
               {entry > 0 &&
                 <label>
@@ -298,11 +280,11 @@ export default function GameOptionsModal() {
                   </label>
               }
               </article> 
-            <select name='rules'>
+            <select name='rules'   onChange={handleRulesChange}>
               <option value=''>Winter or Summer Rules?</option>
               {rulesOptionItems}
             </select>
-            <select name='putts'>
+            <select name='putts' onChange={handlePuttsChange}>
               <option value=''>Putts Good?</option>
               {puttsOptionItems}
             </select>
